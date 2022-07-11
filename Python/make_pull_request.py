b469759ae9a6234ab6f4f3d79aa7c04f67d5566a
Dmitry Nikulin <russiankenny@gmail.com>
2022-07-11 12:41:06 +0300

    feat: added error handling

--- Python/make_pull_request.py
+++ Python/make_pull_request.py
@@ -6,13 +6,17 @@ import requests
 
 auth_key = f'token {os.environ.get("GH_API_KEY")}'
 
-parser = argparse.ArgumentParser()
+if auth_key == None:
+    sys.exit('No GitHub auth key was found.')
+
+parser = argparse.ArgumentParser(
+    description='Create a GitHub pull request from the command line.')
 parser.add_argument('head', help='Specify the head branch name')
 parser.add_argument('base', help='Specify the branch you want to merge into')
 parser.add_argument('owner', help='Specify the repository owner')
 parser.add_argument('repo', help='Specify the repository name')
 parser.add_argument(
-    'pull_title', help='Specify the pull request title, optional.')
+    'pull_title', help='Specify the pull request title.')
 
 args = parser.parse_args()
 
@@ -34,17 +38,30 @@ data = {
 
 payload = json.dumps(data)
 
-if auth_key == None:
-    sys.exit('No GitHub auth key was found.')
+print(f"""
+Repository owner: {args.owner}
+Repository name: {args.repo}
 
-req = requests.post(url=api_url, headers=req_headers, data=payload)
-res = req.json()
+Pull request title: {args.pull_title}
+Merging from: {args.head}
+Merging into: {args.base}
+""")
 
-print(f'Got a response: {req.status_code}')
+confirmation = input('Are you sure you want to create this request? [Y/n]')
 
-if req.status_code == 201:
-    print(f'Pull request URL: {res.get("html_url")}')
-    sys.exit()
+if confirmation == 'Y' or 'y':
+    req = requests.post(url=api_url, headers=req_headers, data=payload)
+    res = req.json()
+
+    print(f'Got a response: {req.status_code}')
+
+    if req.status_code == 201:
+        print(f'Pull request URL: {res.get("html_url")}')
+        sys.exit()
+    else:
+        print(f'Something went wrong: {res}')
+        sys.exit('Operation did not complete.')
+
 else:
-    print(f'Something went wrong: {res}')
-    sys.exit('Operation did not complete.')
+    print(f'"No" selected. Aborting.')
+    sys.exit()
